22d634008221f556d0928922a3ce57f6b874d067
marsfffffff <q1563524035@gmail.com>
2024-10-24 16:14:54 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,4 @@
 # coding: utf-8
-# @email: enoche.chow@gmail.com
-
-"""
-Main entry
-# UPDATED: 2022-Feb-15
-##########################
-"""
 
 import os
 import argparse
@@ -15,7 +8,7 @@ os.environ['NUMEXPR_MAX_THREADS'] = '48'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='SELFCFED_LGN', help='name of models')
+    parser.add_argument('--model', '-m', type=str, default='TEMOO', help='name of models')
     parser.add_argument('--dataset', '-d', type=str, default='baby', help='name of datasets')
     parser.add_argument('--mg', action="store_true", help='whether to use Mirror Gradient, default is False')
 
