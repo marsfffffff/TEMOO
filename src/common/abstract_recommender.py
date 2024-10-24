66c5892b85dc4c78a215beaafc3e1cf11944129a
marsfffffff <q1563524035@gmail.com>
2024-10-24 16:16:20 +0800

    Update abstract_recommender.py

--- src/common/abstract_recommender.py
+++ src/common/abstract_recommender.py
@@ -91,13 +91,13 @@ class GeneralRecommender(AbstractRecommender):
         if not config['end2end'] and config['is_multimodal_model']:
             dataset_path = os.path.abspath(config['data_path'] + config['dataset'])
             # if file exist?
-            # v_feat_file_path = os.path.join(dataset_path, config['vision_feature_file'])
-            # t_feat_file_path = os.path.join(dataset_path, config['text_feature_file'])
-            # if os.path.isfile(v_feat_file_path):
-            #     self.v_feat = torch.from_numpy(np.load(v_feat_file_path, allow_pickle=True)).type(torch.FloatTensor).to(
-            #         self.device)
-            # if os.path.isfile(t_feat_file_path):
-            #     self.t_feat = torch.from_numpy(np.load(t_feat_file_path, allow_pickle=True)).type(torch.FloatTensor).to(
-            #         self.device)
+            v_feat_file_path = os.path.join(dataset_path, config['vision_feature_file'])
+            t_feat_file_path = os.path.join(dataset_path, config['text_feature_file'])
+            if os.path.isfile(v_feat_file_path):
+                self.v_feat = torch.from_numpy(np.load(v_feat_file_path, allow_pickle=True)).type(torch.FloatTensor).to(
+                    self.device)
+            if os.path.isfile(t_feat_file_path):
+                self.t_feat = torch.from_numpy(np.load(t_feat_file_path, allow_pickle=True)).type(torch.FloatTensor).to(
+                    self.device)
 
-            # assert self.v_feat is not None or self.t_feat is not None, 'Features all NONE'
+            assert self.v_feat is not None or self.t_feat is not None, 'Features all NONE'
